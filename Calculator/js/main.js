03da75d694f516c3182d09b8293c3550c448ac81
Alexander <uxanovav@yandex.ru>
2020-09-25 15:48:10 +0400

    feats: add some function & make redisign

--- Calculator/js/main.js
+++ Calculator/js/main.js
@@ -1,17 +1,19 @@
-let numbers = document.querySelectorAll('.button-number');
-let operations = document.querySelectorAll('.button-option');
-let eq = document.getElementById('equ');
-let ac = document.getElementById('ac');
-let c = document.getElementById('c');
-let screen = document.querySelector('.screen-value');
-let resultScreen = document.getElementById('result');
+const numbers = document.querySelectorAll('.button-number');
+const operations = document.querySelectorAll('.button-option');
+const eq = document.getElementById('equ');
+const ac = document.getElementById('ac');
+const c = document.getElementById('c');
+const screen = document.querySelector('.screen-value');
+const resultScreen = document.getElementById('result');
+const sqrt = document.getElementById('sqrt');
+const plmin = document.getElementById('plmin');
 let result = '';
 let current = '';
 let a = '';
 let b = '';
 let action = '';
 
-for ( let i = 0; i < numbers.length; i++){
+for (let i = 0; i < numbers.length; i++) {
     let num = numbers[i];
     num.addEventListener('click', function (e) {
         numberPress(num.id);
@@ -19,16 +21,16 @@ for ( let i = 0; i < numbers.length; i++){
     })
 }
 
-for ( let i = 0; i < operations.length; i++) {
+for (let i = 0; i < operations.length; i++) {
     let ops = operations[i];
-    ops.addEventListener('click', function (e){
+    ops.addEventListener('click', function (e) {
         optionPress(ops.id);
-        result += " "+ ops.innerHTML + " ";
+        result += " " + ops.innerHTML + " ";
         resultScreen.innerHTML = result;
     })
 }
 
-ac.addEventListener('click', function (e){
+ac.addEventListener('click', function (e) {
     current = '';
     screen.value = current;
     a = '';
@@ -38,15 +40,15 @@ ac.addEventListener('click', function (e){
     resultScreen.innerHTML = result;
 });
 
-c.addEventListener('click', function (e){
-    current = current.substring(0, current.length - 1);
+c.addEventListener('click', function (e) {
+    result = resultScreen.innerHTML.substring(0, resultScreen.innerHTML.length - current.length) + " ";
+    current = '';
     screen.value = current;
-    result = resultScreen.innerHTML.substring(0,resultScreen.innerHTML.length - 1);
     resultScreen.innerHTML = result;
 });
 
 eq.addEventListener('click', function (e) {
-    a = onEqPress(a,current,action);
+    a = onEqPress(a, current, action);
     resultScreen.innerHTML = result;
 });
 
@@ -54,86 +56,139 @@ screen.addEventListener('oninput', function (e) {
     resultScreen.innerHTML = screen.value;
 })
 
+sqrt.addEventListener('click', function (e) {
+    optionPress(sqrt.id);
+    if (Number(a) > 0) {
+        onEqPress(a, a, action);
+        result = sqrt.innerText + result;
+        resultScreen.innerHTML = result;
+        result = Math.sqrt(Number(a)).toString();
+    } else {
+        resultScreen.innerHTML = 'Value lower than zero!'
+    }
+})
 
-function numberPress (id) {
-    switch(id){
+plmin.addEventListener('click', function (e) {
+    console.log(current);
+    if (current != '' && current != 0 && !current.includes('-')) {
+        current = '-' + current;
+    } else {
+        current = current.slice(1);
+    }
+    screen.value = current;
+    result = screen.value
+    resultScreen.innerHTML = result;
+})
+
+function numberPress(id) {
+    switch (id) {
         case 'one':
-            current+='1';
-            result+='1';
+            current += '1';
+            result += '1';
             break;
         case 'two':
-            current+='2';
-            result+='2';
+            current += '2';
+            result += '2';
             break;
         case 'three':
-            current+='3';
-            result+='3';
+            current += '3';
+            result += '3';
             break;
         case 'four':
-            current+='4';
-            result+='4';
+            current += '4';
+            result += '4';
             break;
         case 'five':
-            current+='5';
-            result+='5';
+            current += '5';
+            result += '5';
             break;
         case 'six':
-            current+='6';
-            result+='6';
+            current += '6';
+            result += '6';
             break;
         case 'seven':
-            current+='7';
-            result+='7';
+            current += '7';
+            result += '7';
             break;
         case 'eigth':
-            current+='8';
-            result+='8';
+            current += '8';
+            result += '8';
             break;
         case 'nine':
-            current+='9';
-            result+='9';
+            current += '9';
+            result += '9';
             break;
         case 'zzero':
-            current+='0';
-            result+='0';
+            current += '0';
+            result += '0';
             break;
         case 'dott':
-            current+='.';
-            result+='.';
-            break;
+            if (current.includes('.') || current.length === 0) {
+                break;
+            } else {
+                current += '.';
+                result += '.';
+                break;
+            }
     }
     screen.value = current;
 }
 
 function optionPress(id) {
+    if (current != ''){
+        if (current !== a) {
+            b = current;
+        } else {
+            b = a;
+        }
+        onEqPress(a,b,action);
         action = id;
         a = screen.value;
         current = '';
+    }
 }
 
-function onEqPress(a,total,action){
+function onEqPress(a, total, action) {
     b = total;
-    if (action === 'plus'){
-        total = Number(a) + Number(b);
+    if (action === 'plus') {
+        if (!Number.isInteger(Number(a)) || !Number.isInteger(Number(b))) {
+            total = (Number(a) * 100 + Number(b) * 100) / 100;
+            let max = a.length >= b.length ? a : b;
+            if (total.toString().length > max.length + 1) {
+                total = +total.toFixed(max.length);
+            }
+
+        } else {
+            total = Number(a) + Number(b);
+        }
         a = total.toString();
         screen.value = total.toString();
     }
-    if (action === 'minus'){
+    if (action === 'minus') {
         total = Number(a) - Number(b);
         a = total.toString();
         screen.value = total.toString();
     }
-    if (action === 'multiply'){
+    if (action === 'multiply') {
         total = Number(a) * Number(b);
         a = total.toString();
         screen.value = total.toString();
     }
-    if (action === 'divide'){
+    if (action === 'divide') {
         total = Number(a) / Number(b);
         a = total.toString();
         screen.value = total.toString();
     }
+    if (action === 'sqrt') {
+        total = Math.sqrt(a);
+        a = total.toString();
+        screen.value = total.toString();
+    }
+    if (action === 'expo') {
+        total = Math.pow(a, b);
+        a = total.toString();
+        screen.value = total.toString();
+    }
     result += ' = ' + a + ' ';
-    console.log(a);
     return a;
 }
